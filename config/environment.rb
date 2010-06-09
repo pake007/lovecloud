4a0b35403db670d5bf2806a1f7d09be5bf9f8858
jimmy <pake007@gmail.com>
2010-06-09 19:29:24 +0800

    add gem dependencies to config

--- config/environment.rb
+++ config/environment.rb
@@ -15,10 +15,11 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "RedCloth",    :lib => "redcloth", :version => "~> 4.0"
+  config.gem "chronic",  :version => "~> 0.2.0"
+  config.gem "coderay",  :version => "~> 0.8.0"
+  config.gem "lesstile", :version => "~> 0.3"
+  config.gem "will_paginate", :version => "~> 2.3", :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
